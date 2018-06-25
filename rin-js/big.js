89fd49fa9c05893db9efb37b968dec872c3c0cc9
baka <me@nessy.pw>
2018-06-25 23:44:36 +0300

    style rework and samples edits
    reworked styles and code a little bit
    added header and bottom bars with """info"""

---
+++ rin-js/big.js
@@ -0,0 +1,19 @@
+a=[],l=45,p=null,c=null,r={
+    s:function(){
+        a = [];
+        for(var i = 1; i < l+1; i++){
+            a.push(new Audio('./kowai-neko-rin-nya/'+i+'.wav'))
+        }
+    },
+    n:function(){
+        if(p != null) {
+            a[p].pause();
+            a[p].currentTime = 0;
+        }
+        while(c===p){
+            c = Math.floor(Math.random()*l-1)+1;
+        }
+        a[c].play();
+        p=c;
+    }
+}
--- smol-rin.js
+++
@@ -1,1 +0,0 @@
-var a=[],l=45,p=undefined;function uh(){a=[];for(var i=1;i<l+1;i++){a.push(new Audio('./kowai-neko-rin-nya/'+i+'.wav'))}}function nya(){if(p!=undefined){a[p].pause();a[p].currentTime=0}p=Math.floor(Math.random()*l-1)+1;a[p].play()}
